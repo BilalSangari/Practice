521e52dc3afa8db7a4d0e7433bf88022a1982b90
BilalSangari <bilalsangar2003@gmail.com>
2025-09-13 17:09:54 +0430

    more updates in pokemon

--- src/components/hooks/useEffect/HowNotToFetchApi.jsx
+++ src/components/hooks/useEffect/HowNotToFetchApi.jsx
@@ -2,30 +2,49 @@ import { useEffect, useState } from "react";
 import "./Pokemon.css";
 
 export const HowNotToFetchApi = () => {
-    const [apiData, setApiData] = useState(null)
+  const [pokemon, setPokemon] = useState(null);
 
-      const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
+  const API = "https://pokeapi.co/api/v2/pokemon/squirtle";
 
-      const fetchPokemon = () => {
-        fetch(API).then((res) => res.json()).then((data) => {
-            setApiData(data);
-        })
-        .catch((error) => console.log(error))
-      }
+  const fetchPokemon = () => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemon(data);
+      })
+      .catch((error) => console.log(error));
+  };
 
-      useEffect(() => {
-        fetchPokemon();
-      }, []);
-    return(
-        <section className="contianer">
-            <header>
-                <h1>Let's Catch Pokemon</h1>
-            </header>
-            <ul className="card-demo">
-                <li className="pokemon-card">
-                    <figure></figure>
-                </li>
-            </ul>
-        </section>
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  if(!pokemon){
+    return (
+        <div>
+            <h1>Loading....</h1>
+        </div>
     )
-}
+  }
+//   if (pokemon) {
+    return (
+      <section className="contianer">
+        <header>
+          <h1>Let's Catch Pokemon</h1>
+        </header>
+        <ul className="card-demo">
+          <li className="pokemon-card">
+            <figure>
+              <img
+                src={pokemon.sprites.other.dream_world.front_default}
+                alt={pokemon.name}
+                className="pokemon-image"
+              />
+            </figure>
+            <h1>{pokemon.name}</h1>
+          </li>
+        </ul>
+      </section>
+    );
+  }
+
